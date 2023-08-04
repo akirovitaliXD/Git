c969b59cf6efa40d24a4487162480cb78e76a83b
akirovitaliXD <petrov.welel@gmail.com>
2023-08-04 16:41:15 +1000

    0.0.2

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #Импорт модуля для упрощения работы
 import datetime
+import sys
 #Переменные
-a = datetime.date.today()
+a = datetime.datetime.today()
 #Вывод в консоль
-print("Год", a.year)
-print("Месяц", a.month)
-print("День", a.day)
+
+print("Год:", a.year)
+print("Месяц:", a.month)
+print("День:", a.day)
+print("Час:", a.hour)
+print("Минут:", a.minute)
+print("Секунд:", a.second)
+
